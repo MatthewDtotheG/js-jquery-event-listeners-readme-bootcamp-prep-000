b3f3ef2ea981f27da70a1671bcf7daffa43c870f
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-04 15:26:42 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,6 +3,11 @@ function getIt() {
   $('p').on('click');
   alert('Hey!');
 }
+
+function frameIt() {
+  $('p').on('click');
+  alert('Hey!');
+}
 $(document).ready(function() {
   // call functions here
   getIt();
